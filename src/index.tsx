182e068bb9dde73341e2c31e9d81084500b2b110
Zackarias Montell <zackarias@montell.se>
2021-03-13 17:37:20 +0100

    Set up parcel with typescript

---
+++ src/index.tsx
@@ -0,0 +1,5 @@
+import { createElement } from "react";
+import { render } from "react-dom";
+import Root from "./root";
+
+render(createElement(Root), document.getElementById("root"));
---
+++ src/root.tsx
@@ -0,0 +1,7 @@
+import React = require("react");
+
+const Root = () => {
+  return <div>Hejsdsdsdan</div>;
+};
+
+export default Root;
